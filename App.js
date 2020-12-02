dc62fa7aef504cc03b8a53a2e6a0c9048408a181
SergioAriel <solis.sergioariel@gmail.com>
2020-12-02 13:49:37 -0300

    first commit

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ class Product {
 }
 
 class UI{
+    constructor(isShowingAlert){
+        this.isShowingAlert = isShowingAlert = false
+    }
+
     addProduct(product){
         const productList = document.getElementById("product-list");
         const element = document.createElement("div");
@@ -15,31 +19,64 @@ class UI{
             <div class="card text-center mb-4">
                 <div class="card-body">
                     <strong>Product Name</strong>: ${product.name}
-                    <strong>Product Price</strong> ${product.price}
-                    <strong>Product Year</strong> ${product.year}
+                    <strong>Product Price</strong>: ${product.price}
+                    <strong>Product Year</strong>: ${product.year}
+                    <a name="delete" href="#" class="btn btn-danger">Delete</a>
                 </div>
             </div>
        `;
        productList.appendChild(element);
-    }
-    deleteProduct(){
+       
+    };
 
-    }
-    showMessage(){
+    resetForm(){
+        document.getElementById("product-form").reset()
+    };
 
+    deleteProduct(element){
+        if(element.name === "delete"){
+            element.parentElement.parentElement.parentElement.remove()
+            this.showMessage("Product Delete Satisfactory", "danger")
+        }
+        
+    };
+
+    showMessage(message, cssClass){
+        if(this.isShowingAlert!=true){
+            this.isShowingAlert = true;
+            const div = document.createElement("div");
+            div.className = `alert alert-${cssClass}`;
+            div.appendChild(document.createTextNode(message));
+            //Showing in DOM
+            const container = document.querySelector(".container");
+            const app = document.querySelector("#App");
+            container.insertBefore(div, app)    
+            setTimeout(() => {
+                document.querySelector(".alert").remove();
+                this.isShowingAlert = false
+            }, 3000);;
+    };
     }
 }
+//DOM Events
+document.getElementById("product-form").addEventListener("submit", function(){
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const year = document.getElementById("year").value;
+    const product = new Product(name, price, year);
+    const ui= new UI();
 
-document.getElementById("product-form")
-    .addEventListener("submit", function(){
-   const name = document.getElementById("name").value;
-   const price = document.getElementById("price").value;
-   const year = document.getElementById("year").value;
+    if(name==="" || price=== "" || year=== ""){
+        return ui.showMessage("Complete Message", "info");
+    };
+    ui.addProduct(product);
+    ui.resetForm();
+    ui.showMessage("Producto Agregado", 'success')
 
-   const product = new Product(name, price, year);
-   const ui= new UI();
-   ui.addProduct(product);
+   event.preventDefault();
+});
 
-
-   e.preventDefault();
+document.getElementById("product-list").addEventListener('click', function(e){
+    const ui = new UI()
+    ui.deleteProduct(e.target)
 });
